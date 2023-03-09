0b896eed360b94131081bf9c2dc14a3ac5b559b6
CourseMaterial <admin_external_service@educative.io>
2023-03-09 22:29:04 +0500

    second

---
+++ src/main/java/org/example/Search.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Search {
+
+    public static int exponentialSearch(int[] arr, int target) {
+        int n = arr.length;
+        if (n == 0) {
+            return -1; // array is empty
+        }
+        if (arr[0] == target) {
+            return 0;
+        }
+        int i = 1;
+        while (i < n && arr[i] <= target) {
+            i *= 2;
+        }
+        int l = i / 2;
+        int r = i - 1;
+        if (r >= n) {
+            r = n - 1;
+        }
+        while (l <= r) {
+            int temp = (r - l)/2;
+            int m = l + temp;
+            if (arr[m] == target) {
+              return m;
+            } else if (arr[m] < target) {
+                l = m + 1;
+                if (l > r) {
+                    return -1; // target not found
+                }
+      }
+            else {
+                r = m - 1;
+                if (l > r) {
+                    return -1; // target not found
+                }
+            }
+        }
+        return -1; // target not found
+    }
+
+
+    }
---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,103 @@
+import org.example.Search;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+public class SearchTests {
+
+
+    @Test
+    public void testExponentialSearch() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 8;
+        int expectedIndex = 7;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+
+    @Test
+    public void testExponentialSearchFound() {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 5;
+        int expectedIndex = 2;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchNotFound() {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 6;
+        int expectedIndex = -1;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchFirstElement() {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 1;
+        int expectedIndex = 0;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchLastElement() {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 11;
+        int expectedIndex = 5;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchEmptyArray() {
+        int[] arr = {};
+        int target = 10;
+        int expectedIndex = -1;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchDuplicateElements() {
+        int[] arr = {1, 3, 5, 5, 7, 9, 11};
+        int target = 5;
+        int expectedIndex = 2;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertTrue(actualIndex == expectedIndex || actualIndex == 3);
+    }
+
+    @Test
+    public void testExponentialSearchNegativeElements() {
+        int[] arr = {-11, -9, -7, -5, -3, -1};
+        int target = -5;
+        int expectedIndex = 3;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchZeroElement() {
+        int[] arr = {1, 3, 0, 7, 9, 11};
+        int target = 0;
+        int expectedIndex = -1;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    public void testExponentialSearchLargeArray() {
+        int[] arr = new int[1000000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        int target = 999999;
+        int expectedIndex = 999999;
+        int actualIndex = Search.exponentialSearch(arr, target);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+}
